0368cd9cd66701cfaa702f2d76dc2c96f39b1c4a
kallekaa <kalle.kaarlela@gmail.com>
2025-09-18 20:30:08 +0300

    first qr code test successful

--- main.py
+++ main.py
@@ -1,11 +1,44 @@
 import os
 from dotenv import load_dotenv
 
+from langchain import hub
+from langchain_openai import ChatOpenAI
+from langchain.agents import create_react_agent, AgentExecutor
+from langchain_experimental.tools import PythonREPLTool
+
 load_dotenv()
 
 def main():
     print("Hello from code-interpreter-clone!")
 
+    instructions = """You are an agent designed to write and execute python code to answer questions.
+    You have access to a python REPL, which you can use to execute python code.
+    You have qrcode package installed
+    If you get an error, debug your code and try again.
+    Only use the output of your code to answer the question. 
+    You might know the answer without running any code, but you should still run the code to get the answer.
+    If it does not seem like you can write code to answer the question, just return "I don't know" as the answer.
+        """
+    base_prompt = hub.pull("langchain-ai/react-agent-template")
+    prompt = base_prompt.partial(instructions=instructions)
+
+    tools = [PythonREPLTool()]
+    python_agent = create_react_agent(
+        prompt=prompt,
+        llm=ChatOpenAI(temperature=0, model="gpt-4o-mini"),
+        tools=tools,
+    )
+
+    python_agent_executor = AgentExecutor(agent=python_agent, tools=tools, verbose=True,
+                                          handle_parsing_errors=True)
+
+    print(
+        python_agent_executor.invoke(
+            {
+                "input": "Generate and save in current working directory 3 qrcodes that point to `www.langchain.com`",
+            }
+        )
+    )
 
 if __name__ == "__main__":
     main()
